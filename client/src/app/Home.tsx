9fdac42a0127df89b9842db05fb438e1d6bcfc5c
Akansha Bisht <akansha7803@gmail.com>
2026-09-08 22:06:49 +0530

    client added

---
+++ client/src/app/Home.tsx
@@ -0,0 +1,44 @@
+"use client";
+import { useState } from "react";
+
+export default function Home() {
+  const [tab, setTab] = useState(1);
+  const [task, setTask] = useState(null);
+  const handleTabs = (tab: number) => {
+    setTab(tab);
+    // console.log(tab);
+  }
+
+  const handleAddTask = (e)=> {
+    e.preventDefault();
+    // console.log(task);
+  }
+  return <div className=" w-screen h-screen">
+    <div className="flex flex-col w-screen h-screen justify-center items-center">
+      <div>
+      <h2 className="font-bold text-2xl">Mini Task Board</h2>
+    </div>
+    <div className="flex flex-row gap-2 mt-4">
+      <input value={task} onChange={(e) => setTask(e.target.value)} type="text" placeholder="Add a new task..." className=" w-60 p-2 outline-none border border-gray-100 rounded-md"/>
+    <button onClick={handleAddTask} className="cursor-pointer" >Add Task  </button>
+    </div>
+    <div className="flex text-md w-80 justify-evenly mt-4 cursor-pointer">
+      <p onClick={() => handleTabs(1)} className={`${tab === 1 ? "text-white" : "text-gray-500"} cursor-pointer`}>Todos</p>
+      <p onClick={() => handleTabs(2)} className={`${tab === 2 ? "text-white" : "text-gray-500"} cursor-pointer`}>In Progress</p>
+      <p onClick={() => handleTabs(3)} className={`${tab === 3 ? "text-white" : "text-gray-500"} cursor-pointer`}>Done</p>
+    </div>
+    <div className="flex justify-between flex-row gap-4 mt-4 border border-gray-100 p-3 rounded-md w-80">
+    <div>
+      <p className="text-lg font-semibold">Buy rice</p>
+      <p className="text-sm text-gray-500">Due: Today</p>
+      <p className="text-sm text-gray-500">Status: Active</p>
+    </div>
+    <div className="flex flex-col gap-2 justify-start items-start ">
+      <button className="text-yellow-500 cursor-pointer">Edit</button>
+      <button className="text-red-500 cursor-pointer">Delete</button>
+      <button className="text-green-500 cursor-pointer">Done</button>
+    </div>
+    </div>
+    </div>
+  </div>;
+}
